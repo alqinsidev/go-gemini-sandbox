cee802225ab3bb214be732487127caba87154736
alqinsidev <padlanalqinsi@live.com>
2024-02-08 17:57:01 +0700

    feat: add chat request validation

--- src/delivery/http/chat.go
+++ src/delivery/http/chat.go
@@ -4,14 +4,21 @@ import (
 	"net/http"
 
 	"github.com/alqinsidev/go-gemini-sandbox/src/domain"
+	_error "github.com/alqinsidev/go-gemini-sandbox/src/error"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) GetChatResponse(ctx *gin.Context) {
 	var body domain.ChatRequestPayload
-	ctx.Bind(&body)
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": _error.ErrBadPayloadRequest.Error(),
+		})
+		return
+	}
 
-	answer, err := h.Usecase.GetChatResponse(ctx, body.Input)
+	answer, err := h.Usecase.GetChatResponse(ctx, body.Question)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"success": false,
@@ -23,8 +30,8 @@ func (h *Handler) GetChatResponse(ctx *gin.Context) {
 	response := gin.H{
 		"success": true,
 		"data": domain.ChatResponse{
-			Input:  body.Input,
-			Output: answer,
+			Question: body.Question,
+			Answer:   answer,
 		},
 	}
 	ctx.JSON(http.StatusOK, response)
