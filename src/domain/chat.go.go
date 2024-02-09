3b92e732846447a1910af3fa7c04a8cea0906eb4
alqinsidev <padlanalqinsi@live.com>
2024-02-09 13:15:40 +0700

    chore: remove unused struct

--- src/domain/chat.go.go
+++ src/domain/chat.go.go
@@ -1,18 +1,5 @@
 package domain
 
-import (
-	"github.com/google/generative-ai-go/genai"
-)
-
-type GenAI struct {
-	Client         *genai.Client
-	PreDefinedPart []genai.Part
-}
-
-type Part struct {
-	Text string `json:"text"`
-}
-
 type ChatRequestPayload struct {
 	Question string `json:"question" binding:"required"`
 }
